a7d0173a636e0ef427138625e1c8d799c05d2415
Jared Kosanovic <kosanovj@oregonstate.edu>
2017-08-03 11:01:41 -0700

    create method to get string from properties

--- src/main/groovy/edu/oregonstate/mist/inventory/ErrorMessages.groovy
+++ src/main/groovy/edu/oregonstate/mist/inventory/ErrorMessages.groovy
@@ -4,49 +4,49 @@ import edu.oregonstate.mist.api.Error
 
 class ErrorMessages {
     static Error invalidUUID() {
-        Error.badRequest(Error.prop.getProperty('inventory.invalidUUID'))
+        Error.badRequest(Error.getFromProperties('inventory.invalidUUID'))
     }
 
     static Error idExists() {
-        Error.badRequest(Error.prop.getProperty('inventory.idExists'))
+        Error.badRequest(Error.getFromProperties('inventory.idExists'))
     }
 
     static Error castError() {
-        Error.badRequest(Error.prop.getProperty('inventory.castError'))
+        Error.badRequest(Error.getFromProperties('inventory.castError'))
     }
 
     static Error badType(String value, String allowedTypes) {
         Error.badRequest(
-                "${value} ${Error.prop.getProperty('inventory.badType')} ${allowedTypes}")
+                "${value} ${Error.getFromProperties('inventory.badType')} ${allowedTypes}")
     }
 
     static Error noType(String allowedTypes) {
         Error.badRequest(
-                "${Error.prop.getProperty('inventory.noType')} ${allowedTypes}")
+                "${Error.getFromProperties('inventory.noType')} ${allowedTypes}")
     }
 
     static Error otherType() {
-        Error.badRequest(Error.prop.getProperty('inventory.otherType'))
+        Error.badRequest(Error.getFromProperties('inventory.otherType'))
     }
 
     static Error badSourceType(String value, String allowedSourceTypes) {
         Error.badRequest(
-                "${value} ${Error.prop.getProperty('inventory.badSourceType')}" +
+                "${value} ${Error.getFromProperties('inventory.badSourceType')}" +
                         " ${allowedSourceTypes}"
         )
     }
 
     static Error noSourceType(String allowedSourceTypes) {
         Error.badRequest(
-                "${Error.prop.getProperty('inventory.noSourceType')} ${allowedSourceTypes}"
+                "${Error.getFromProperties('inventory.noSourceType')} ${allowedSourceTypes}"
         )
     }
 
     static Error otherSourceType() {
-        Error.badRequest(Error.prop.getProperty('inventory.otherSourceType'))
+        Error.badRequest(Error.getFromProperties('inventory.otherSourceType'))
     }
 
     static String getInventoryError() {
-        Error.prop.getProperty('inventory.getInventoryError')
+        Error.getFromProperties('inventory.getInventoryError')
     }
 }
