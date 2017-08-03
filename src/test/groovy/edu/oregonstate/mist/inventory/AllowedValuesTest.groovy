4d1f7c268d5f34ae740331ab668755326efa8e54
Jared Kosanovic <kosanovj@oregonstate.edu>
2017-08-03 12:54:52 -0700

    test edge cases

--- src/test/groovy/edu/oregonstate/mist/inventory/AllowedValuesTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/inventory/AllowedValuesTest.groovy
@@ -4,19 +4,44 @@ import org.junit.Test
 
 class AllowedValuesTest {
 
-    private AllowedValues testValues = new AllowedValues(list: ['foo', 'bar', 'eggplant'])
-
     @Test
-    public void testPrettyString() {
-        String prettyString = testValues.pretty()
-        List<String> parsedPrettyString = prettyString.tokenize(',')
+    public void testOverTwoItemPrettyString() {
+        List<String> testValues = ['bob', 'john', 'frank']
+        List<String> parsedPrettyString = getPrettyStringFromList(testValues).tokenize(',')
 
-        testValues.list.each {
-            if (it == testValues.list[-1]) {
+        testValues.each {
+            if (it == testValues[-1]) {
                 assert "or " + it == parsedPrettyString[-1].trim()
             } else {
-                assert it == parsedPrettyString[testValues.list.indexOf(it)].trim()
+                assert it == parsedPrettyString[testValues.indexOf(it)].trim()
             }
         }
+    }
+
+    @Test
+    public void testTwoItemPrettyString() {
+        List<String> testValues = ['bob', 'frank']
+
+        assert  getPrettyStringFromList(testValues) == testValues[0] + " or " + testValues[1]
+    }
+
+    @Test
+    public void testOneItemPrettyString() {
+        List<String> testValues = ['bob']
+
+        assert getPrettyStringFromList(testValues) == testValues[0]
+    }
+
+    @Test
+    public void testZeroItemPrettyString() {
+        List<String> testValues = []
+
+        assert !getPrettyStringFromList(testValues)
+    }
+
+    private String getPrettyStringFromList(List<String> list) {
+        AllowedValues testValues = new AllowedValues(list: list)
+
+        testValues.pretty()
     }
 }
